a56add07a58ede944ba12b6101d69c14e6c28095
MeetKakani334 <kakanimeet2@gmail.com>
2024-06-22 01:50:48 -0700

    hmk

---
+++ clockmeaker/web.js
@@ -0,0 +1,14 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+let cartItem = document.querySelector('.cart');
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+}
