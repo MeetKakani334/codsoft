ab0bb2751882d9b046b3435782ab44105277d944
MeetKakani334 <kakanimeet2@gmail.com>
2024-07-04 06:15:04 -0700

    hii

---
+++ python/radius.py
@@ -0,0 +1,8 @@
+a = float(input("enter valu"))
+b = 3.14
+c = 2
+d = b * (a ** c)
+e = c* a
+print(" the re:" , d)
+print(" the de:" , e)
+
---
+++ python/squareofnumber.py
@@ -0,0 +1,2 @@
+a = int(input("Enter Your Intvalu : "))
+print("The square Of Intvalu is : ", a ** 2 )
