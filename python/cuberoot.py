e866c1329e66435424abbec9daaccbbb48f1262a
MeetKakani334 <kakanimeet2@gmail.com>
2024-07-12 08:49:43 -0700

    ho

--- python/cuberoot.py
+++ python/cuberoot.py
@@ -1,2 +1,6 @@
 cube = float(input("Enter The Valu of Cube :- "))
-print(cube ** (1/3))
+print(round (cube ** (0.333333333) , 2))
+
+# root = math.pow(cube,1/3)
+
+# (** is the in bilud function similur to power function  power function x = pow(4,3,5) the mining of 4*4*4%5)
