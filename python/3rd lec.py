451343917b255e0df0ab8c14d351e469e6b8cdef
MeetKakani334 <kakanimeet2@gmail.com>
2024-07-17 04:28:55 -0700

    hii

---
+++ python/3rd lec.py
@@ -0,0 +1,28 @@
+# Tuple
+
+# tuple is a immutable
+
+# Ex
+a= (1,2,3,4,5,6,7,8,9,10)
+print (a)
+print (a[4:])
+print (a[:6])
+print (a[4:6])
+print (a[:])
+
+b = (11,12,13,14,15,16,17,18,19,20)
+
+c = a + b
+print (c)
+
+d = list(a)
+print (d)
+
+e = list(a + b)
+print (e)
+
+f = a * 2
+print (f)
+
+
+
---
+++ python/solvingTuple.py
@@ -0,0 +1,14 @@
+# 1)
+if True:
+    print("Hello, World!")
+# 2)
+# print(x)
+#   is not defined
+# 3)
+name = "John"
+age = 30
+print("My name is " + name + "And My Age Is" + str(age))
+# 4)
+my_list = [1, 2, 3]
+print(my_list[5])
+# out of rang
