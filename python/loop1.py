a505421d78698c32f782ad5d03980a299c2aaff5
MeetKakani334 <kakanimeet2@gmail.com>
2024-07-28 21:00:16 -0700

    Added the loop files

---
+++ python/loop1.py
@@ -0,0 +1,11 @@
+# a = 4
+# for i in range(a):
+#     print(" "*i + "*"*(a))
+
+# n = 4
+# for i in range(4,n+1):
+#     print("* "*n+" "*(n+i))
+
+n = 4 
+for i in range(n):
+    print(" "*i+"*"*n )
---
+++ python/loop3.py
@@ -0,0 +1,28 @@
+# rows = 4
+# col = 4
+# for i in range(rows):
+#     print(" "*(rows-i-1)+"*"*(2*i+4))
+#     for j in range(col):
+#         print(" "*(col+j)+"*"*(2*j-4))
+        
+
+# rows = 4
+# for i in range(rows):
+#     print(" "*(rows-i-1)+"*"*(2*i+4))
+#     # for j in range(rows):
+#     #     print(" "*(rows+j+1)+"*"*(2*j+4))
+#     # for j in range(rows):
+#     #     print(" "*(rows+j)+"*"*(2*j-2))
+    # for j in range(8 , rows-1+2):
+    #     print(" "*(rows-j-1)+"*"*(2*j+4))
+
+
+rows = 4
+for i in range(rows):
+    print(" "*(rows-i-1)+"*"*(2*i+4))
+for i in range(rows-2, -1, -1):
+    print(" "*(rows-i-1)+"*"*(2*i+4))
+
+
+
+        
