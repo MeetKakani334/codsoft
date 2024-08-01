c85559ccd3770b400f205816b38c0cf4fc99027b
MeetKakani334 <kakanimeet2@gmail.com>
2024-08-01 08:25:31 -0700

    merge the latest code

---
+++ python/list.py
@@ -0,0 +1,15 @@
+def bi(n):
+    a, b = 0, 1
+    for i in range(n):
+        if i == 0:
+            arry.append(a)
+        elif i == 1:
+            arry.append(b)
+        else:
+            # c = a + b
+            a , b = b , a + b
+            arry.append(a)
+n = 10
+arry = []
+bi(n)
+print(arry)
