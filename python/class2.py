4b32addd0311fb01ca6b95fbf25610dc261fd7d9
MeetKakani334 <kakanimeet2@gmail.com>
2024-09-16 22:19:11 +0530

    its new

--- python/class2.py
+++ python/class2.py
@@ -16,23 +16,71 @@
 # my_book.book_d()
 
 
-class Bank_Account:
-    account_holder =""
-    deposit = 0
-    withdraw = 0
-    balance = 0
+# class Bank_Account:
+#     account_holder =""
+#     deposit = 0
+#     withdraw = 0
+#     balance = 0
 
-    def Account(self):
-        print(f"{self.account_holder},{self.balance}")
+#     def Account(self):
+#         print(f"{self.account_holder},{self.balance}")
 
-account_d = Bank_Account()
+# account_d = Bank_Account()
 
-account_d.account_holder = "A"
-account_d.deposit = 45000
-account_d.withdraw = 10000
-account_d.balance = 35000
+# account_d.account_holder = "A"
+# account_d.deposit = 45000
+# account_d.withdraw = 10000
+# account_d.balance = 35000
 
 
-account_d.Account()
+# account_d.Account()
+
+
+
+# class library:
+#     Add = ""
+#     Remove = ""
+
+#     def lib(self):
+#         print(f"Callection of library is {self.Add}")
+
+# remove_lib = library()
+# remove_lib.Add = "A,B,C,D,F"
+# remove_lib.Remove = "E,G,H"
+
+# remove_lib.lib()
+
+
+# class shoping_cart :
+#     item1 = ""
+#     prise1 = 0
+#     item2 = ""
+#     prise2 = 0
+#     item3 = ""
+#     prise3 = 0 
+
+#     def list(self):
+#         print(f"This is your Cart {self.item1},{self.item2}")
+#         total_pric = self.prise1 + self.prise2
+#         print(f"this is the total price :- {total_pric}")
+
+# your_cart = shoping_cart()
+# your_cart.item1 = "[Chips"
+# your_cart.prise1 = 10
+# your_cart.item2 = "Milk]"
+# your_cart.prise2 = 30
+
+
+# your_cart.list()
+
+
+# class student :
+#     name = ""
+#     roll_number = ""
+#     grades = {}
+
+#     def methods(self):
+
+        
 
 
