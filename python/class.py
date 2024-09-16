f88464196c4d3ebccc76ae9dfe4a29ecf772eef2
MeetKakani334 <kakanimeet2@gmail.com>
2024-09-16 21:30:18 +0530

    hii

--- python/class.py
+++ python/class.py
@@ -16,30 +16,45 @@
 
 
 class result:
+    Name = ''
     Math = 0.0
     Eng = 0.0
     SS = 0.0
     Sci = 0.0
     Guj = 0.0
     Snk = 0.0
+
     def shoe_result(self):
-        print(f"Show Result:Math:- {self.Math}, Eng:- {self.Eng}, SS:- {self.SS}, Sci:- {self.Sci}, Guj:- {self.Guj}, Snk:- {self.Snk}")
+        print(f"Show Result:\n Math:- {self.Math},\n Eng :- {self.Eng},\n SS  :- {self.SS},\n Sci :- {self.Sci},\n Guj :- {self.Guj},\n Snk :- {self.Snk}\n")
         total_mark = self.Math + self.Eng + self.SS + self.Sci + self.Guj + self.Snk
-        per = (total_mark/700)*100
-        print(f"Total Mark Is {total_mark}")
-        print(f"Percentage: {per:.2f}%")
+        print("This Is Your Final Markshit\n")
+        per = (total_mark/600)*100
+        print(f"Your Total Mark Is:- {total_mark}")
+        print(f"Your Percentage Is:- {per:.2f}%")
+        if per <= 100 and per > 90:
+            print("Congratulations Your Grad is (A+)")
+        elif per <= 90 and per > 80:
+            print("Congratulations Your Grad is (A)")
+        elif per <= 80 and per > 70:
+            print("Congratulations Your Grad is (B+)")
+        elif per <= 70 and per > 60:
+            print("Congratulations Your Grad is (B)")
+        elif per <= 60 and per > 50:
+            print("Congratulations Your Grad is (C+)")
+        elif per <= 50 and per > 40:
+            print("Congratulations Your Grad is (C)")
+        elif per <= 40 and per > 33:
+            print("Congratulations Your Grad is (D)")
+        else:
+            print("Soory You Are Fali Please Try Again Better Luck For Next Time")
+            
 my_result = result()
-
-my_result.Math = float(input("Enter Your Mathematics Mark Out Of 100:-"))
-my_result.Eng  = float(input("Enter Your English Mark Out Of 100:-"))
-my_result.SS   = float(input("Enter Your Social Science Mark Out Of 100:-"))
-my_result.Sci  = float(input("Enter Your Science Mark  Out Of 100:-"))
-my_result.Guj  = float(input("Enter Your Gujarati (FL) Mark  Out Of 100:-"))
-my_result.Snk  = float(input("Enter Your Sanskrit (SL) Mark  Out Of 100:-"))
-
+my_result.Name = print("Please Enter The Mark's According To SubJect")
+my_result.Math = float(input("Enter Your Mathematics Mark Out Of 100   :- "))
+my_result.Eng  = float(input("Enter Your English Mark Out Of 100       :- "))
+my_result.SS   = float(input("Enter Your Social Science Mark Out Of 100:- "))
+my_result.Sci  = float(input("Enter Your Science Mark  Out Of 100      :- "))
+my_result.Guj  = float(input("Enter Your Gujarati (FL) Mark  Out Of 100:- "))
+my_result.Snk  = float(input("Enter Your Sanskrit (SL) Mark  Out Of 100:- "))
 
 my_result.shoe_result()
-
-
-
-
